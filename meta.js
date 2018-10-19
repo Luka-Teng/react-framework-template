9bcb83067ac2e23e02d44a7e5244dcb6afeb9def
Luka <luka.teng@ximalaya.com>
2018-10-19 14:45:18 +0800

    auto run

--- meta.js
+++ meta.js
@@ -1,3 +1,10 @@
+const {
+  sortDependencies,
+  installDependencies,
+  printMessage
+} = require('./utils')
+const path = require('path')
+
 module.exports = {
   prompts: {
     name: {
@@ -26,11 +33,34 @@ module.exports = {
           value: 'redux'
         }
       ]
+    },
+    autoInstall: {
+      type: 'confirm',
+      message: '是否要帮您自动运行"npm install"在构建完成之后'
     }
   },
   filters: {
     'src/router/**/*': 'router',
     'src/store/**/*': "store && storeConfig === 'mobx'",
     'src/redux/**/*': "store && storeConfig === 'redux'"
+  },
+  complete: (data, { chalk }) => {
+    const green = chalk.green
+
+    sortDependencies(data, green)
+
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+
+    if (data.autoInstall) {
+      installDependencies(cwd, data.autoInstall, green)
+        .then(() => {
+          printMessage(data, green)
+        })
+        .catch(e => {
+          console.log(chalk.red('Error:'), e)
+        })
+    } else {
+      printMessage(data, chalk)
+    }
   }
 }
---
+++ utils.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const fs = require('fs')
+const spawn = require('child_process').spawn
+
+/**
+ * 重新排列package.json依赖
+ */
+exports.sortDependencies = function sortDependencies (data) {
+  const packageJsonFile = path.join(
+    data.inPlace ? '' : data.destDirName,
+    'package.json'
+  )
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonFile))
+  if (packageJson.devDependencies) {
+    packageJson.devDependencies = sortObject(packageJson.devDependencies)
+  }
+  if (packageJson.dependencies) {
+    packageJson.dependencies = sortObject(packageJson.dependencies)
+  }
+  fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2) + '\n')
+}
+
+function sortObject (object) {
+  const sortedObject = {}
+  Object.keys(object)
+    .sort()
+    .forEach(item => {
+      sortedObject[item] = object[item]
+    })
+  return sortedObject
+}
+
+/**
+ * 安装依赖
+ * @param {string} cwd 项目地址
+ */
+exports.installDependencies = function installDependencies (
+  cwd,
+  executable,
+  color
+) {
+  console.log(`\n\n# ${color('Installing project dependencies ...')}`)
+  console.log('# ========================\n')
+  return runCommand(executable ? 'npm' : 'none', ['install'], {
+    cwd
+  })
+}
+
+/**
+ * 输出最后的完成信息.
+ * @param {Object} data metadata.
+ */
+exports.printMessage = function printMessage (data, { green, yellow }) {
+  const message = `
+# ${green('Project initialization finished!')}
+# ========================
+To get started:
+  ${yellow(
+    `${data.inPlace ? '' : `cd ${data.destDirName}\n  `}${installMsg(
+      data
+    )}npm run dev`
+  )}
+`
+  console.log(message)
+}
+
+function installMsg (data) {
+  return !data.autoInstall ? 'npm install (or if using yarn: yarn)\n  ' : ''
+}
+
+/**
+ * Spawn一个子进程用来执行shell命令
+ * @param {string} cmd 命令
+ * @param {array<string>} args 参数
+ * @param {object} options 选项，cwd: 命令执行目录...
+ */
+function runCommand (cmd, args, options) {
+  return new Promise((resolve, reject) => {
+    const spwan = spawn(
+      cmd,
+      args,
+      Object.assign(
+        {
+          cwd: process.cwd(),
+          stdio: 'inherit',
+          shell: true
+        },
+        options
+      )
+    )
+
+    spwan.on('exit', () => {
+      resolve()
+    })
+  })
+}
